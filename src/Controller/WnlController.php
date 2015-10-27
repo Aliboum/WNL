71c073720cb3ba7b1a050162917f86d9eeccab12
Aliboum <boumlalali@gmail.com>
2015-10-27 18:19:09 +0000

    Conf_DB_1

---
+++ src/Controller/WnlController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class WnlController extends AppController   
+{
+    public function index()
+    {
+        echo"hello";
+    }
+}
+?>
